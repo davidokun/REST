962d5e750450a3d81fdc70e5c02fc026e306eec6
David Marin <davidokun99@gmail.com>
2015-10-13 22:38:59 -0500

    Add Spark configuration

--- spark-mongo-project/spark-mongo-app/src/main/java/com/davidokun/App.java
+++ spark-mongo-project/spark-mongo-app/src/main/java/com/davidokun/App.java
@@ -1,13 +1,17 @@
 package com.davidokun;
 
+
+import static spark.Spark.get;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+
+        get("/hello", (req, res) -> "Hello World Spark and Mongo");
+
     }
 }
